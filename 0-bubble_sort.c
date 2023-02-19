8c617866d088713fee82034fb193ba1e03aa3656
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-02-19 01:20:14 +0100

    hot fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,3 +25,19 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
+
+/**
+ * swap_arr - utility function to swap 2 array elements
+ * @arr: pointer to the array to be swapped
+ * @idx1: first index
+ * @idx2: second index
+ */
+void swap_arr(int **arr, size_t idx1, size_t idx2)
+{
+        int temp, *array;
+
+        array = *arr;
+        temp = array[idx1];
+        array[idx1] = array[idx2];
+        array[idx2] = temp;
+}
