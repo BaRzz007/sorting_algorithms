baa8905c1a427bd5e1e758c02b4939e56ac6ba94
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-02-19 17:57:43 +0100

    done quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,86 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using quick sort
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	_quick_sort(&array, size, 0, size - 1);
+}
+
+/**
+ * _quick_sort - internal working quick sort function
+ * @array: array to be sorted
+ * @size: size of the array
+ * @left: lower bound of the array
+ * @right: upper bound of the array
+ */
+void _quick_sort(int **array, size_t size, int left, int right)
+{
+	int pivot_idx, *arr;
+
+	arr = *array;
+	pivot_idx = partition(&arr, size, left, right);
+	if (left < pivot_idx - 1)
+		_quick_sort(&arr, size, left, pivot_idx - 1);
+	if (pivot_idx + 1 < right)
+		_quick_sort(&arr, size, pivot_idx + 1, right);
+}
+
+/**
+ * partition - partitions the array around the chosen pivot
+ * @array: array to be partitioned
+ * @size: size of the array
+ * @left: lower bound
+ * @right: upper bound
+ * Return: returns the final position of the pivot
+ */
+int partition(int **array, size_t size, int left, int right)
+{
+	int *arr, pivot, leftmark, rightmark;
+
+	arr = *array;
+	pivot = arr[right];
+
+	leftmark = left;
+	rightmark = right - 1;
+	while (leftmark <= rightmark)
+	{
+		while (leftmark <= rightmark && arr[leftmark] <= pivot)
+			leftmark++;
+		while (leftmark <= rightmark && arr[rightmark] >= pivot)
+			rightmark--;
+		if (leftmark < rightmark)
+		{
+			swap_arr(&arr, leftmark, rightmark);
+			leftmark++;
+			rightmark--;
+			print_array(arr, size);
+		}
+	}
+	if (leftmark != right)
+	{
+		swap_arr(&arr, leftmark, right);
+		print_array(arr, size);
+	}
+
+	return (leftmark);
+}
+
+/**
+ * swap_arr - utility function to swap 2 array elements
+ * @arr: pointer to the array to be swapped
+ * @idx1: first index
+ * @idx2: second index
+ */
+void swap_arr(int **arr, size_t idx1, size_t idx2)
+{
+	int temp, *array;
+
+	array = *arr;
+	temp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = temp;
+}
--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void swap_arr(int **, size_t, size_t);
 void swap_list(listint_t **, listint_t **);
 void bubble_sort(int *, size_t);
 void selection_sort(int *, size_t);
+void quick_sort(int *, size_t);
+void _quick_sort(int **, size_t, int, int);
+int partition(int **, size_t, int, int);
 
 #endif
