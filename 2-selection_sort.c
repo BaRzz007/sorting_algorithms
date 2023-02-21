377f5e434fb60cc3cde4cbf172d6312d1b92996c
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-02-21 13:04:25 +0100

    fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,7 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
@@ -18,7 +18,7 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
-					min_idx = j;
+				min_idx = j;
 		}
 		if (min_idx != i)
 		{
