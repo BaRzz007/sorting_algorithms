bd45173b4626650724ae853dd23511d97e20b63f
Frankenstei <enwereloveday@yahoo.com>
2023-02-21 09:43:58 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,45 +1,31 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * @array: array to be sorted
- * @size: size of the array
+ * bubble_sort - bubble sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: nothing
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t j;
-	int is_sorted;
+	size_t i, j;
+	int temp;
 
-	if (size > 1)
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
 	{
-		is_sorted = FALSE;
-		while (!is_sorted)
+		for (j = 0; j < size; j++)
 		{
-			is_sorted = TRUE;
-			for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap_arr(&array, j, j + 1);
-					is_sorted = FALSE;
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
-}
-/**
- * swap_arr - utility function to swap 2 array elements
- * @arr: pointer to the array to be swapped
- * @idx1: first index
- * @idx2: second index
- */
-void swap_arr(int **arr, size_t idx1, size_t idx2)
-{
-        int temp, *array;
-
-        array = *arr;
-        temp = array[idx1];
-        array[idx1] = array[idx2];
-        array[idx2] = temp;
 }
